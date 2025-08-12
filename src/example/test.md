8cd8b1d915729877df4899906e089baa98b6d275
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-12 09:41:31 +0900

    wip

--- src/example/test.md
+++ src/example/test.md
@@ -18,7 +18,13 @@ $ yarn add -D vitepress
 
 :::
 
-## Alert
+## Custom container
+
+::: tip
+By default, VitePress stores its dev server cache in `.vitepress/cache`, and the production build output in `.vitepress/dist`. If using Git, you should add them to your `.gitignore` file. These locations can also be [configured](../reference/site-config#outdir).
+:::
+
+## Github flavored alert
 
 > [!TIP]
 > By default, VitePress stores its dev server cache in `.vitepress/cache`, and the production build output in `.vitepress/dist`. If using Git, you should add them to your `.gitignore` file. These locations can also be [configured](../reference/site-config#outdir).
--- src/lib/plugins/markdown.ts
+++ src/lib/plugins/markdown.ts
@@ -92,6 +92,12 @@ export function markdownPlugin(): Plugin[] {
 	];
 }
 
+// https://vitepress.dev/guide/markdown#custom-containers
+const CUSTOM_BLOCKS = ["info", "tip", "warning", "danger", "details"];
+
+// https://vitepress.dev/guide/markdown#github-flavored-alerts
+const GITHUB_ALERTS = ["note", "tip", "important", "warning", "caution"];
+
 function remarkCustom() {
 	return function (tree: Root, file: VFile) {
 		visit(tree, function (node) {
@@ -192,8 +198,44 @@ function remarkCustom() {
 					];
 					return;
 				}
+				if (CUSTOM_BLOCKS.includes(node.name)) {
+					// add custom-block class
+					// const data = node.data || (node.data = {});
+					// const tagName = "div";
+					// data.hName = tagName;
+					// data.hProperties = h(tagName, node.attributes || {}).properties;
+					// data.hProperties["class"] = `custom-block ${node.name}`;
+					// // process label
+					// const directiveLabel = node.children[0];
+					// let label = node.name.toUpperCase();
+					// if (directiveLabel.data && "directiveLabel" in directiveLabel.data) {
+					// 	label = (directiveLabel as any).children[0].value;
+					// 	node.children.shift();
+					// }
+					// const labelNode = {
+					// 	type: "paragraph",
+					// 	children: [
+					// 		{
+					// 			type: "text",
+					// 			value: label,
+					// 		},
+					// 	],
+					// 	data: {
+					// 		hName: "p",
+					// 		hProperties: {
+					// 			class: "custom-block-title",
+					// 		},
+					// 	},
+					// };
+					// node.children.unshift(labelNode as any);
+					return;
+				}
 				file.info("Unknown directive: " + node.name);
 			}
+
+			// TODO
+			// https://vitepress.dev/guide/markdown#github-flavored-alerts
+			GITHUB_ALERTS;
 		});
 	};
 }
