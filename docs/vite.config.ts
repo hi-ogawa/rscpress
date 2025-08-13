0ee0e26a0a44ea7477e999bfe20b8f434f98bbde
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-13 23:18:36 +0900

    chore: tweak deps

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,6 +1,6 @@
+import rscpress from "@hiogawa/rscpress/plugin";
 import { defineConfig } from "vite";
 import inspect from "vite-plugin-inspect";
-import rscpress from "@hiogawa/rscpress/plugin";
 
 export default defineConfig({
 	plugins: [rscpress(), inspect()],
